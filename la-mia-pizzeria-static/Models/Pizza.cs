0943ccd71b2eb02d8a5de475e924432a4ab5efad
L-V-Dragon <viotti37@gmail.com>
2023-01-17 14:34:35 +0100

    Add project files.

---
+++ la-mia-pizzeria-static/Models/Pizza.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace la_mia_pizzeria_static.Models
+{
+    public class Pizza
+    {
+        public int ID { get; set; }
+
+		[Column(TypeName = "varchar(100)")]
+        [StringLength(100, ErrorMessage = "Il campo titolo non può contenere più di 100 caratteri")]
+        public string Nome{ get; set; }
+
+		[Column(TypeName = "varchar(300)")]
+        [StringLength(300, ErrorMessage = "Il campo titolo non può contenere più di 300 caratteri")]
+        public string Foto { get; set; }
+
+		[Column(TypeName = "text")]
+		public string Descrizione { get; set; }
+
+		[Column(TypeName = "varchar(10)")]
+        [StringLength(10, ErrorMessage = "Il campo titolo non può contenere più di 10 caratteri")]
+        public string Prezzo { get; set; }
+
+        public Pizza()
+        {
+
+        }
+
+        public Pizza(int id, string nome, string foto, string descrizione, string prezzo)
+        {
+            ID = id; 
+            Nome = nome;
+            Foto = foto;
+            Descrizione = descrizione;
+            Prezzo = prezzo;
+        }
+    }
+}
